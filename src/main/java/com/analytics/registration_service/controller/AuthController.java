027193e01121e34ebb064f533feb294bbc0dd9eb
iamvirendra <iamvirendra412@gmail.com>
2026-07-10 18:29:30 +0530

    authcontroller done

--- src/main/java/com/analytics/registration_service/controller/AuthController.java
+++ src/main/java/com/analytics/registration_service/controller/AuthController.java
@@ -4,16 +4,18 @@ import com.analytics.registration_service.dto.request.LoginRequest;
 import com.analytics.registration_service.dto.request.RefreshTokenRequest;
 import com.analytics.registration_service.dto.request.RegisterRequest;
 import com.analytics.registration_service.dto.response.AuthResponse;
+import com.analytics.registration_service.entity.User;
 import com.analytics.registration_service.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -43,4 +45,32 @@ public class AuthController {
         return ResponseEntity.ok(response);
     }
 
+    //logout
+    @PostMapping("/logout")
+    public ResponseEntity<Map<String, String>> logout(Authentication authentication){
+        User user  = (User) authentication.getPrincipal();
+        authService.logout(user);
+        return ResponseEntity.ok(Map.of("message", "Logged out Successfully"));
+    }
+
+    //Invalidate all tokens (Admin Only)
+    @PostMapping("/invalidate/{userId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Map<String, String>> invalidateAllTokens(@PathVariable Long userId){
+        authService.invalidateAllToken(userId);
+        return ResponseEntity.ok(Map.of("message", "All tokens invalidated for userId: " + userId));
+    }
+
+    //get current user
+    @GetMapping("/me")
+    public ResponseEntity<Map<String, String>> getCurrentUser(Authentication authentication){
+        User user = (User) authentication.getPrincipal();
+
+        return ResponseEntity.ok(Map.of(
+                "email", user.getEmail(),
+                "firstName", user.getFirstName(),
+                "lastName", user.getLastName(),
+                "role", user.getRole().name()
+        ));
+    }
 }
--- src/main/java/com/analytics/registration_service/dto/request/RefreshTokenRequest.java
+++ src/main/java/com/analytics/registration_service/dto/request/RefreshTokenRequest.java
@@ -1,7 +1,9 @@
 package com.analytics.registration_service.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
+@Data
 public class RefreshTokenRequest {
 
     @NotBlank(message = "token can't be empty")
